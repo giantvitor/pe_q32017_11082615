5542327a75d30c19128147aa71234fb12af6d2cf
giantvitor <32200199+giantvitor@users.noreply.github.com>
2017-09-23 19:05:46 -0300

    Add files via upload

---
+++ lista01/02.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    int x;
+    int y;
+    int soma;
+
+    scanf("%d", &x);
+    scanf("%d", &y);
+
+    soma = x+y;
+    printf("%d", soma);
+
+    return 0;
+}
