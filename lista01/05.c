5de91fbe19c75f526cb10eb3c9c1a28c84748944
giantvitor <32200199+giantvitor@users.noreply.github.com>
2017-09-23 19:21:20 -0300

    Add files via upload

---
+++ lista01/05.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+    int x;
+    int y;
+    int div;
+
+    scanf("%d", &x);
+    scanf("%d", &y);
+
+    div = x / y;
+
+    printf("%d", div);
+
+    return 0;
+}
