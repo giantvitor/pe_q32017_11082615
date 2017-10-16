e536a84f3f2961744c09f33650e9ac571210e629
giantvitor <32200199+giantvitor@users.noreply.github.com>
2017-10-16 09:56:58 -0200

    Add files via upload

---
+++ lista02/13.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+
+
+unsigned int mdc(unsigned int m, unsigned int n)
+
+{
+  
+	if(n==0) return m;
+  
+	return mdc(n,(m%n));
+
+}
+
+
+
+int main()
+
+{
+  
+ 
+	unsigned int m; unsigned int n;
+
+  
+  
+	scanf("%d" "%d", &m, &n);
+  
+
+  
+	printf("%d", mdc(m,n));
+  
+
+  
+	return 0;
+
+}
