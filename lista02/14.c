8b96ee9f39e9baaaee8ef921c3bea5fb78770301
giantvitor <32200199+giantvitor@users.noreply.github.com>
2017-10-19 15:28:06 -0200

    Add files via upload

---
+++ lista02/14.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+
+
+int palindromo(int n, int resto)
+
+{
+  
+  
+
+	if (n == 0) return resto;
+  
+	return palindromo((n / 10), (resto * 10) + (n % 10));
+  
+  
+
+} 
+
+
+
+int main ()
+
+{
+
+  
+  
+	int n; int resto = 0;
+  
+  
+	
+	scanf("%d", &n);
+  
+  
+
+	if (palindromo(n, resto) == n) printf("sim\n");
+  
+	else printf("nao\n");
+    
+  
+
+	return 0;
+
+
+}  
