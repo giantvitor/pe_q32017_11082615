023bd77d7eac2d459c060862b7bbe28b6f63c8a5
giantvitor <32200199+giantvitor@users.noreply.github.com>
2017-10-19 09:59:11 -0200

    Add files via upload

---
+++ lista02/6.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+#include <math.h>
+
+
+
+double delta(double a, double b, double c)
+
+{
+  
+  
+	return (pow(b,2)) - (4*a*c);
+
+}
+
+
+
+double bhaskara_2(double a, double b, double c)
+
+{
+  
+	if (delta(a,b,c) < 0) return (sqrt((-1)*delta(a,b,c))) / (2*a);
+
+	return ((-1*b) - sqrt(delta(a,b,c))) / (2*a);
+
+}
+
+
+
+double bhaskara_1(double a, double b, double c)
+
+{
+ 
+	if (delta(a,b,c) < 0) 
+	return (sqrt((-1)*delta(a,b,c))) / (2*a);
+  
+	return ((-1*b) + sqrt(delta(a,b,c))) / (2*a);
+
+}
+
+
+
+int main() 
+{
+  
+    
+
+	double a; double b; double c; double d;
+    
+    
+	scanf("%lf %lf %lf", &a, &b, &c);
+    
+   
+ 
+	if (delta(a,b,c) < 0) printf("%.2lf - %.2lf i %.2lf + %.2lf i\n", (-1*b)/(2*a), bhaskara_1(a,b,c), (-1*b)/(2*a), bhaskara_2(a,b,c));
+    
+	else printf("%.2lf %.2lf\n", bhaskara_2(a,b,c), bhaskara_1(a,b,c));
+    
+    
+
+	return 0;
+}
