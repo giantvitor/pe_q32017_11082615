bfa77d0691bd148a2f49ab6672fe81c1dc47b02a
giantvitor <32200199+giantvitor@users.noreply.github.com>
2017-10-19 16:30:17 -0200

    Add files via upload

---
+++ lista02/12.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+
+
+int coef_binomial(int n, int k)
+
+{
+  
+
+	if(n == k || k == 0) return 1;
+  
+	return coef_binomial((n-1), (k-1)) + coef_binomial((n-1), k);
+
+
+}
+
+
+
+int main()
+
+{
+  
+	int n; int k;
+  
+  
+
+	scanf("%d" "%d", &n, &k);
+  
+  
+
+	printf("%d\n", coef_binomial(n, k));
+  
+  
+
+	return 0;
+
+}
